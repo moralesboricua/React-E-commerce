928a0f2a2e0a20a19c83442daf9755f31a3d3e9d
moralesboricua <moralesboricua@gmail.com>
2026-05-13 15:14:47 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import Nav from './components/Nav';
 import Landing from './components/Landing';
 import Highlights from './components/Highlights'; 
+import Featured from './components/Featured';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Nav />
       <Landing />
       <Highlights />
+      <Featured />
     </div>
   );
 }
--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,10 +1,24 @@
+
 import React from 'react'
+import Book from './ui/Book';
 
 function Featured() {
   return (
-    <div>
-        Featured
-    </div>
+    <section id="features">
+        <div className="container">
+            <div className="row">
+                <h2 className="section__title">
+                    Featured <span className="purple">Books</span>
+                </h2>
+                <div className="books">
+                    <Book />
+                    <Book />
+                    <Book />
+                    <Book />
+                </div>
+            </div>
+        </div>
+    </section>
   )
 }
 
