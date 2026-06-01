f8d9c05a446ee09bc071f0a4895c4ed5d5fd79f4
moralesboricua <moralesboricua@gmail.com>
2026-06-01 18:12:21 -0400

    updated

--- src/components/Discounted.jsx
+++ src/components/Discounted.jsx
@@ -11,7 +11,10 @@ function Discounted() {
             Discount <span className="purple">Books</span>
           </div>
           <div className="books">
-            {books.map((book) => (
+            {books
+            .filter(book => book.salePrice > 0)
+            .slice(0, 8)
+            .map((book) => (
               <Book book={book} key={book.id} />
             ))}
           </div>
